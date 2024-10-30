82ce5279bfce26bb3dad63df1a08c3db446416db
Zombotheprogrmmer <zombotheprogrmmer@gmail.com>
2024-10-30 19:37:22 +0330

    add line count functionality

--- wc/__main__.py
+++ wc/__main__.py
@@ -1,6 +1,36 @@
 import argparse
 from os.path import isfile, exists
 
+def get_details_string(path, details, state):
+    output = ""
+    if state & 1:
+        output += f'{details["lines"]:4} '
+    if path:
+        output += path
+    return output
+
+def get_file_details(file_path):
+    details = {
+        "lines": 0,
+    }
+    fh = open(file_path)
+    for line in fh:
+        for char in line:
+            if char == "\n":
+                details["lines"] += 1
+    fh.close()
+    return details
+
+def get_program_state(args):
+    """
+    additive state calculator
+    1 => lines
+    """
+    state = 0
+    if args.lines:
+        state |= 1
+    return state
+
 def get_path_state(file_path):
     """
     0 path doesn't exist
@@ -17,16 +47,19 @@ def main():
     # create the command line argument parser and add arguments
     parser = argparse.ArgumentParser(description="a recreation of the Unix wc program by Zombotheprogrmmer. Print newline, word, and byte counts for each FILE, and a total line if more than one FILE is specified. A word is a non-zero-length sequence of printable characters delimited by white space.")
     parser.add_argument("FILE", type=str, nargs="*", help="The adress of file(s) to be proccessed. With no FILE, or when FILE is -, read standard input.")
-    
+    parser.add_argument("-l", "--lines", action="store_true", help="print the newline counts")
     # parse the arguments
     args = parser.parse_args()
-    for file in args.FILE:
-        state = get_path_state(file)
-        if state == 0:
+    program_state = get_program_state(args)
+    for path in args.FILE:
+        path_state = get_path_state(path)
+        if path_state == 0:
             print(f"no such file or directory: {file}")
-        elif state == -1:
+        elif path_state == -1:
             print(f"{file} is a directory")
         else:
-            pass
+            file_details = get_file_details(path)
+            detail_string = get_details_string(path, file_details, program_state)
+            print(detail_string)
 
 main()
